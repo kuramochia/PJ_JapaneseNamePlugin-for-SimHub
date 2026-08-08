99342369999e342934fa4f9c555ab9ac60f78a76
kuramochia <kuramochia@live.jp>
2026-08-09 08:41:58 +0900

    都市名の日本語翻訳データを ATS でも利用できるように (#2)

---
+++ IPluginAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kuramochia.PJ_JapaneseNamePlugin
+{
+    public interface IPluginAction
+    {
+        Task InitAsync(CancellationToken cancellationToken);
+        Task UpdateAsync(CancellationToken cancellationToken = default);
+        void DataUpdate();
+    }
+}
--- PJLocalization.cs
+++ PJLocalization.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Kuramochia.PJ_JapaneseNamePlugin
 {
-    public class PJLocalization
+    public class PJLocalization : IPluginAction
     {
         private const string JobCitySourcePropertyName = "Job.CitySource";
         private const string JobCityDestinationPropertyName = "Job.CityDestination";
@@ -15,14 +15,7 @@ namespace Kuramochia.PJ_JapaneseNamePlugin
         private const string JobCitySourceNoCompletionPropertyName = "Job.CitySource.NoCompletion";
         private const string JobCityDestinationNoCompletionPropertyName = "Job.CityDestination.NoCompletion";
 
-
-        private const string JobJapaneseCitySourcePropertyName = "Job.Ja.CitySource";
-        private const string JobJapaneseCityDestinationPropertyName = "Job.Ja.CityDestination";
-
         private readonly PJ_JapaneseNamePlugin _plugin;
-
-        private HttpClient _httpclient = new HttpClient();
-
         public PJLocalization(PJ_JapaneseNamePlugin plugin)
         {
             _plugin = plugin;
@@ -37,8 +30,6 @@ namespace Kuramochia.PJ_JapaneseNamePlugin
             _plugin.PluginManager.AddProperty(JobCompanySourcePropertyName, _plugin.GetType(), "", "PJ Map では日本語の配送元企業名、それ以外は通常の配送元企業名");
             _plugin.PluginManager.AddProperty(JobCompanyDestinationPropertyName, _plugin.GetType(), "", "PJ Map では日本語の配送先企業名、それ以外は通常の配送先企業名");
 
-            _plugin.PluginManager.AddProperty(JobJapaneseCitySourcePropertyName, _plugin.GetType(), "", "日本語に翻訳された配送元都市名");
-            _plugin.PluginManager.AddProperty(JobJapaneseCityDestinationPropertyName, _plugin.GetType(), "", "日本語に翻訳された配送先都市名");
             await UpdateAsync(cancellationToken);
         }
 
@@ -96,7 +87,7 @@ namespace Kuramochia.PJ_JapaneseNamePlugin
             }
         }
 
-        internal void DataUpdate()
+        public void DataUpdate()
         {
             if (_plugin.PluginManager.GameName == "ETS2")
             {
@@ -120,11 +111,6 @@ namespace Kuramochia.PJ_JapaneseNamePlugin
 
                 string companySourceName = string.Empty;
                 string companyDestinationName = string.Empty;
-
-
-                // 都市名の翻訳タスク
-                var translatedCitySourceTask = TryGetTranslatedCityNameAsync(defaultCitySourceName);
-                var translatedCityDestinationTask = TryGetTranslatedCityNameAsync(defaultCityDestinationName);
 
                 // 都市名を取得する
                 _plugin.Settings.Cities?.TryGetValue(citySourceId, out citySourceName);
@@ -161,58 +147,8 @@ namespace Kuramochia.PJ_JapaneseNamePlugin
                 _plugin.PluginManager.SetPropertyValue(JobCompanySourcePropertyName, _plugin.GetType(), companySourceName);
                 _plugin.PluginManager.SetPropertyValue(JobCompanyDestinationPropertyName, _plugin.GetType(), companyDestinationName);
 
-                // 翻訳された都市名を設定
-                _plugin.PluginManager.SetPropertyValue(JobJapaneseCitySourcePropertyName, _plugin.GetType(), translatedCitySourceTask.Result.translatedCityName);
-                _plugin.PluginManager.SetPropertyValue(JobJapaneseCityDestinationPropertyName, _plugin.GetType(), translatedCityDestinationTask.Result.translatedCityName);
             }
         }
 
-        private async Task<(bool found, string translatedCityName)> TryGetTranslatedCityNameAsync(string cityName)
-        {
-            if (string.IsNullOrEmpty(cityName))
-            {
-                return (false, string.Empty);
-            }
-            if (_plugin.TranslatedSettings.TranslatedCities.TryGetValue(cityName, out var translated))
-            {
-                return (true, translated);
-            }
-
-            try
-            {
-                // Google Translate の非公開 API を使用して都市名を日本語に翻訳する
-                var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ja&dt=t&q={System.Net.WebUtility.UrlEncode(cityName)}";
-                // HttpClient は頻繁に呼び出されるので、クラスのフィールドとして保持して再利用
-                var response = await _httpclient.GetAsync(url);
-                if (!response.IsSuccessStatusCode)
-                {
-                    return (false, cityName);
-                }
-
-                var body = await response.Content.ReadAsStringAsync();
-
-                // 取得成功。INFO としてログ出力しておく
-                SimHub.Logging.Current.Info($"PJ_JapaneseNamePlugin Translate Success. {cityName} -> {body}");
-
-                var arr = Newtonsoft.Json.Linq.JArray.Parse(body);
-                // こんな Json が返却される
-                // [[["アテネ","Αθήνα",null,null,11,null,null,[[]],[[["466914b2b9b759682681a550c00b67dd","en_ja_2023q1.md"]]]]],null,"el",null,null,null,1,[],[["el"],null,[1],["el"]]]
-                var translatedText = arr?[0]?[0]?[0]?.ToString() ?? string.Empty;
-
-                if (!string.IsNullOrEmpty(translatedText))
-                {
-                    // 翻訳結果を設定として保存する
-                    _plugin.TranslatedSettings.TranslatedCities[cityName] = translatedText;
-                    SimHub.Logging.Current.Info($"PJ_JapaneseNamePlugin Translate City Added. {cityName} -> {translatedText}");
-                    return (true, translatedText);
-                }
-            }
-            catch (Exception ex)
-            {
-                SimHub.Logging.Current.Error("PJ_JapaneseNamePlugin Translate Error.", ex);
-            }
-
-            return (false, cityName);
-        }
     }
 }
